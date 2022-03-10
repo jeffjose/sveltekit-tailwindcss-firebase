ad48993a258c9e45c138fed235ea492078b49a3b
Jeffrey Jose <jeffjosejeff@gmail.com>
2022-03-10 13:45:03 -0800

    WIP: Copied over rxjs and updated for ts

---
+++ src/lib/firebase/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+	apiKey: 'AIzaSyC9UugHQ5XUfJh7zstCHWhsaLI-CUg2V4E',
+	authDomain: 'sveltekit-tailwindcss-firebase.firebaseapp.com',
+	projectId: 'sveltekit-tailwindcss-firebase',
+	storageBucket: 'sveltekit-tailwindcss-firebase.appspot.com',
+	messagingSenderId: '426350209712',
+	appId: '1:426350209712:web:94013239850ee9b735e975'
+};
